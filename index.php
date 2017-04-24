e4bdf300bedccaa9be637cd54e29b687977a1eb5
GalaxyDefender <qgerard.gerard@gmail.com>
2017-04-24 15:30:47 +0200

    added table via php and SQL

--- index.php
+++ index.php
@@ -1,57 +1,10 @@
 <?php
 
-/*INSTEAD OF USING MYSQL, I'M JUST USING ARRAYS TO FETCH MY DATA
-MYSQL WILL BE USED! */
+include("connection.php");
 
-//include("connection.php");
-//
-//$query = "SELECT * FROM players";
-//$result = mysqli_query($conn, $query);
+$query = "SELECT * FROM players";
+$result = mysqli_query($conn, $query);
 
-        $p1P = 3;
-        $p1Pn = 0;
-        $p1W = 0;
-        $p1Wn = 0;
-        $p1D = 0;
-        $p1Dn = 0;
-        $p1L = 0;
-        $p1Ln = 0;
-        $p1C = 0;
-        $p1Cn = 0;
-
-    $players = array
-        (
-        /*
-            Array is as follows:
-            - Player ID
-            - Player Name
-            - Player Games Played
-            - Player Wins
-            - Player Draws
-            - Player Losses
-            - Total Prometium Caches
-        */
-            array(1, "Quentin Gerard", $p1P, 0, 0, 0, 0),
-            array(2, "John Doe", 0, 0, 0, 0, 0),
-            array(3, "Jane Doe", 0, 0, 0, 0, 0),
-            array(4, "Jim Doe", 0, 0, 0, 0, 0)
-        );
-
-
-
-       $p2P = 0;
-       $p2W = 0;
-       $p2D = 0;
-       $p2L = 0;
-       $p2C = 0;
-
-        $newScore = $players[1][1] + 2;
-        echo $newScore;
-
-    if(isset($_POST["addResult"])){
-
-
-    }
 
 ?>
 
@@ -80,36 +33,8 @@ MYSQL WILL BE USED! */
             Shadow War Campaign
         </h1>
 
-        <?php
-
-        /* INSTEAD OF USING MYSQL, I'M JUST USING ARRAYS TO FETCH MY DATA
-        MYSQL WILL BE USED! */
-
-//            if  (mysqli_num_rows($result) > 0) {
-//    // we have data:
-//    //output the data
-//
-//    echo  "<table class='table table-bordered'><tr><th>ID</th><th>Player Name</th><th>Army Faction</th><th>Email</th></tr>";
-//
-//    while($row = mysqli_fetch_assoc($result)){
-//        echo "<tr><td>".$row["id"]."</td><td>".$row["player_name"]."</td><td>".$row["army_faction"]."</td><td>".$row["email"]."</td></tr>";
-//    }
-//
-//    echo "</table>";
-//
-//} else {
-//    echo "No results!";
-//}
-//
-//mysqli_close($conn);
-
-
-
-        ?>
-
         <h2>List of Players</h2>
-
-        <table class="table table-bordered table-hover">
+           <table class="table table-bordered table-hover">
              <tr>
                 <th>Player Name</th>
                 <th>P</th>
@@ -119,18 +44,29 @@ MYSQL WILL BE USED! */
                 <th>Promethium Caches</th>
             </tr>
             <?php
-                foreach($players as $player){
-                    echo "<tr class='$player[0]'>";
-                    echo "<td>".$player[1]."</td>";
-                    echo "<td>".$player[2]."</td>";
-                    echo "<td>".$player[3]."</td>";
-                    echo "<td>".$player[4]."</td>";
-                    echo "<td>".$player[5]."</td>";
-                    echo "<td>".$player[6]."</td>";
-                    echo "</tr>";
+                if  (mysqli_num_rows($result) > 0) {
+                    // we have data:
+                    //output the data
+
+
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<tr><td>".$row["name"]."</td><td>"
+                        .$row["played"]."</td><td>"
+                        .$row["wins"]."</td><td>"
+                        .$row["draws"]."</td><td>"
+                        .$row["losses"]."</td><td>"
+                        .$row["caches"]."</td></tr>";
                 }
+                    echo "</table>";
+
+                } else {
+                    echo "No results!";
+                }
+
+               mysqli_close($conn);
             ?>
-        </table>
+
+
 
         <form action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF'] ); ?>" method="post">
         <?php
